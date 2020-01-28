f369280d893782cbe7f57dbb80e82e7baac41dc5
James Rogers <jarogers095@gmail.com>
2020-01-28 05:39:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,12 @@
 def hello_t(array)
   if block_given? do
-  i = 0
-  
-  while i < array.length do
-    yield array[i]
-    i += 1
+    i = 0
+    
+    while i < array.length do
+      yield array[i]
+      i += 1
+    end
   end
-  
   return array
 end
 
